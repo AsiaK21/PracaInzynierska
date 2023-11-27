8887672e72f43d0ced24b1584b6670b1a506a743
AsiaK21 <asiak21@onet.eu>
2023-11-27 10:15:05 +0100

    Web naprawiony, koniec projektu

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'user/login_screen.dart';
 import 'user/register_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
 
